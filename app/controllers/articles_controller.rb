94187396802f4d8d68597bc8be395d382c2ab805
manwithgills <chris.dalton1207@gmail.com>
2018-02-11 15:48:31 +0000

    removed redundancies for application controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     def index
         @articles = Article.all
     end
@@ -9,7 +11,6 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
     end
     
     def update
@@ -38,11 +39,9 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "The Article has been deleted."
         redirect_to articles_path
@@ -50,8 +49,11 @@ class ArticlesController < ApplicationController
     
     
     
-    private
+private
     
+    def set_article
+        @article = Article.find(params[:id])
+    end
     def article_params
         params.require(:article).permit(:title, :description)
     end
